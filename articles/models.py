e979b3b25f116b54579f48fc74bf0f8b86002011
ar.jelokhani80 <ar.jelokhani80@gmail.com>
2024-06-18 16:59:25 +0330

    Add article and rating models

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    average_score = models.FloatField(default=0.0)
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    score = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
 
 
