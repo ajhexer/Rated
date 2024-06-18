8ca93c84c3ec8f757e8fbafc461a83be2706ac45
ar.jelokhani80 <ar.jelokhani80@gmail.com>
2024-06-18 20:22:23 +0330

    Add rating serializer

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Rating
 
 
 class ArticleSerializer(serializers.ModelSerializer):
@@ -12,3 +12,14 @@ class ArticleCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ['title', 'text']
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ['article', 'score']
+
+    def validate_score(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Rating score must be between 1 and 5")
+        return value
