e492b4a226af124137bb07e6956ff9a571fdb89c
ar.jelokhani80 <ar.jelokhani80@gmail.com>
2024-06-18 23:26:05 +0330

    Add articles admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+admin.site.register(Article, ArticleAdmin)
